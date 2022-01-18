7a07cd3f218aaf9774cc3c5b1ada6baec23220c8
Yevhen <92868360+ekodenko@users.noreply.github.com>
2022-01-18 11:55:52 +0200

    Initial Commit

---
+++ GSD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSD
+//
+//  Created by Admin on 18.01.2022.
+//  Copyright © 2022 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
